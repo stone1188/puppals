f5780a9188ad254ee78773958b691b3619f0362a
stone1188 <stone1188@yahoo.com>
2024-02-11 14:02:01 -0500

    added hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import {puppyList} from './data.js'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
-console.log(puppyList)
+// const [puppies, setPuppies] = useState(puppyList)
+// console.log(puppies)
   return (
     <>
       <div>
-       
+       <h1>hello world</h1>
       </div>
       
     </>
