cea3f524b79a1d39979776ec6cb77e2d5dffaeca
stone1188 <stone1188@yahoo.com>
2024-02-11 14:32:54 -0500

    removed hello world

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,19 @@ import './App.css'
 
 function App() {
 const [puppies, setPuppies] = useState(puppyList)
-console.log(puppies)
+console.log(puppies);
   return (
     <>
-      <div>
-       <h1>hello world</h1>
+      <div className='App'>
+       
         { 
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
-          })
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
         }
-      </div>
-      
+      </div>   
     </>
-  )
+  );
 }
 
 export default App
